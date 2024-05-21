b28f5d58b21d18966e99e9d3769553a973bceb41
Raja Kolli <rajadilipkolli@gmail.com>
2024-05-21 13:23:44 +0000

    feat : adds test containers in dev mode

---
+++ src/test/java/com/booking/superride/SuperRideApplicationTests.java
@@ -0,0 +1,12 @@
+package com.booking.superride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TestSuperRideApplication.class)
+public class SuperRideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
