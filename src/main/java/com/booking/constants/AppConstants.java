4fb1702cf08316075af7e01d8e925843e56c08d8
Agniraj <126744684+agniraj09@users.noreply.github.com>
2024-06-15 12:54:43 +0530

    Fixed the folder structure - Moved all sub-folders to parent folder

--- src/main/java/com/booking/constants/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.booking.superride.constants;
-
-public class AppConstants {
-
-    public static final String GENERIC_TAXI_NOT_FOUND_ERROR = "Available taxi cannot be found";
-    public static final String ALL_TAXIS_ARE_IN_TRIP_ERROR = "All taxis are on trip. No taxi is available";
-    public static final String NO_ACTIVE_TAXI_FOUND_ERROR = "No active taxi found. All taxis are currently inactive";
-}
