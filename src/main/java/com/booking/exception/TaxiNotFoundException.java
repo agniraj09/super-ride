2d66b280c180da77312ad706c568bcb498d82a4d
Agniraj <126744684+agniraj09@users.noreply.github.com>
2024-06-19 18:44:40 +0530

    Fixed folder structure (#10)
    * Fixed the folder structure - Moved all sub-folders to parent folder
    
    * feat : improve speed of execution of Integration Tests (#8) (#9)
    
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>
    
    ---------
    
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>

--- src/main/java/com/booking/exception/TaxiNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.booking.superride.exception;
-
-
-public class TaxiNotFoundException extends RuntimeException {
-    public TaxiNotFoundException(String message){
-        super(message);
-    }
-}
---
+++ src/main/java/com/booking/superride/exception/TaxiNotFoundException.java
@@ -0,0 +1,8 @@
+package com.booking.superride.exception;
+
+
+public class TaxiNotFoundException extends RuntimeException {
+    public TaxiNotFoundException(String message){
+        super(message);
+    }
+}
