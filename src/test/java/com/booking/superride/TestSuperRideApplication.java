a34d382c48e065678788bec472e077d6e9d0fbab
Agniraj <126744684+agniraj09@users.noreply.github.com>
2024-06-19 18:40:49 +0530

    feat : improve speed of execution of Integration Tests (#8) (#9)
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>

--- src/test/java/com/booking/superride/TestSuperRideApplication.java
+++ src/test/java/com/booking/superride/TestSuperRideApplication.java
@@ -2,19 +2,13 @@ package com.booking.superride;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.springframework.context.annotation.Import;
+import com.booking.superride.common.ContainersConfig;
 
 @TestConfiguration(proxyBeanMethods = false)
+@Import(ContainersConfig.class)
 public class TestSuperRideApplication {
 
-  @Bean
-  @ServiceConnection
-  PostgreSQLContainer<?> postgresContainer() {
-    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.3-alpine"));
-  }
 
   public static void main(String[] args) {
     SpringApplication.from(SuperRideApplication::main).with(TestSuperRideApplication.class).run(args);
