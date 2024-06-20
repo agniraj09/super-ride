633428804f2f97c1fdc7925df33c3992353d10a4
Agniraj <126744684+agniraj09@users.noreply.github.com>
2024-06-20 16:31:59 +0530

    Added test cases for Ride Controller

---
+++ src/test/java/com/booking/superride/controller/RideControllerTest.java
@@ -0,0 +1,114 @@
+package com.booking.superride.controller;
+
+import static com.booking.superride.constants.AppConstants.ALL_TAXIS_ARE_IN_TRIP_ERROR;
+import static com.booking.superride.constants.AppConstants.NO_ACTIVE_TAXI_FOUND_ERROR;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.booking.superride.common.ContainersConfig;
+import com.booking.superride.domain.RideBookingRequest;
+import com.booking.superride.domain.RideDetailsResponse;
+import com.booking.superride.entity.CustomerDetails;
+import com.booking.superride.entity.TaxiDetails;
+import com.booking.superride.repository.CustomerRepository;
+import com.booking.superride.repository.RideRepository;
+import com.booking.superride.repository.TaxiRepository;
+import io.restassured.http.ContentType;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(ContainersConfig.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class RideControllerTest {
+
+    private TaxiDetails taxi;
+    private CustomerDetails customer;
+
+    @Autowired TaxiRepository taxiRepository;
+
+    @Autowired RideRepository rideRepository;
+
+    @Autowired CustomerRepository customerRepository;
+
+    @BeforeAll
+    void setup() {
+        deleteAllData();
+        this.customer = customerRepository.save(new CustomerDetails().setCustomerName("Agniraj"));
+        this.taxi =
+                taxiRepository.save(
+                        new TaxiDetails()
+                                .setMake("Tata")
+                                .setDriverName("Arul")
+                                .setTaxiNumber("TN 59 BH 8191")
+                                .setCurrentLocation('A')
+                                .setStatus("Active"));
+    }
+
+    @Test
+    @Order(1)
+    void testBookRide() {
+        var request =
+                new RideBookingRequest(customer.getCustomerId(), 'A', 'B', LocalDateTime.now());
+        var response =
+                given().contentType(ContentType.JSON)
+                        .body(request)
+                        .when()
+                        .post("/ride/book-ride")
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .extract()
+                        .as(RideDetailsResponse.class);
+        assertTrue(response.rideDetails().rideId() > 0);
+        assertEquals(response.rideDetails().taxiId(), taxi.getTaxiId());
+        assertEquals(response.rideDetails().customerId(), customer.getCustomerId());
+    }
+
+    @Test
+    @Order(2)
+    void testBookRideWithNoAvailableTaxi() {
+        var request =
+                new RideBookingRequest(customer.getCustomerId(), 'A', 'B', LocalDateTime.now());
+        given().contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/ride/book-ride")
+                .then()
+                .statusCode(404)
+                .body("detail", equalTo(ALL_TAXIS_ARE_IN_TRIP_ERROR));
+    }
+
+    @Test
+    @Order(3)
+    void testBookRideWithNoActiveTaxi() {
+        // Change taxi status to inactive first
+        taxiRepository.save(taxi.setStatus("Inactive"));
+
+        var request =
+                new RideBookingRequest(customer.getCustomerId(), 'A', 'B', LocalDateTime.now());
+        given().contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/ride/book-ride")
+                .then()
+                .statusCode(404)
+                .body("detail", equalTo(NO_ACTIVE_TAXI_FOUND_ERROR));
+    }
+
+    private void deleteAllData() {
+        rideRepository.deleteAll();
+        customerRepository.deleteAll();
+        taxiRepository.deleteAll();
+    }
+
+    @AfterAll
+    void cleanUp() {
+        deleteAllData();
+    }
+}
--- src/test/java/com/booking/superride/controller/TaxiControllerTest.java
+++ src/test/java/com/booking/superride/controller/TaxiControllerTest.java
@@ -1,10 +1,12 @@
 package com.booking.superride.controller;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 import com.booking.superride.common.ContainersConfig;
 import com.booking.superride.domain.AddTaxiRequest;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -16,14 +18,40 @@ public class TaxiControllerTest {
     @Test
     void testSaveTaxiDetailsWithSingleTaxi() {
         var request = new AddTaxiRequest("Tata", "TN 59 BH 8191", "Arul", 'A');
-        RestAssured.given()
-                .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
                 .body(request)
                 .when()
                 .post("/taxi/add-taxi")
                 .then()
                 .statusCode(200)
-                .body("taxiId", Matchers.greaterThan(0))
-                .body("taxiNumber", Matchers.equalTo(request.taxiNumber()));
+                .body("taxiId", greaterThan(0))
+                .body("taxiNumber", equalTo(request.taxiNumber()));
+    }
+
+    @Test
+    void testSaveTaxiDetailsWithInvalidTaxiNumber() {
+        var request = new AddTaxiRequest("Tata", null, "Arul", 'A');
+        given().contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/taxi/add-taxi")
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    void testSaveTaxiDetailsWithMultipleTaxis() {
+        var request =
+                List.of(
+                        new AddTaxiRequest("Tata", "TN 59 BH 8191", "Arul", 'A'),
+                        new AddTaxiRequest("Maruti Suzuki", "TN 12 JK 1718", "Guru", 'A'));
+        given().contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/taxi/bulk/add-taxi")
+                .then()
+                .statusCode(200)
+                .body(".", isA(List.class))
+                .body(".", hasSize(2));
     }
 }
